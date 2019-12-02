2ec05fb8e7f64abc402e19fd0d4702091e7e0915
heyong <macx@macx.cn>
2019-12-02 20:41:18 +0800

    first commit

---
+++ cmdshelljs.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/**
+ * author: heyong.
+ * description: 命令行ui交互
+ * create date: heyong on 2019/12/2
+ * updater: none.
+ * update date: heyong on 2019/12/2
+ */
+// 由于node支持require，所以使用require，开头声明node可执行程序
+const url = require('./urlconfig.js')
+console.log(url)
---
+++ urlconfig.js
@@ -0,0 +1,14 @@
+/**
+ * author: heyong.
+ * description: 专用文件夹配置地址.
+ * create date: heyong on 2019/12/2
+ * updater: none.
+ * update date: heyong on 2019/12/2
+ */
+// 单独提出来一个地址变量，俩个变量初始目录 和 目标目录
+const URL = {
+  original:'', // 初始目录
+  target:'' // 目标目录
+}
+
+module.exports = URL
